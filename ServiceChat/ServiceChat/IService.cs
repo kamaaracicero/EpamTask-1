968bf701cebeb742d5fbbf20af6b8becc1794019
kamaaracicero <verovkin02@gmail.com>
2020-12-26 22:37:17 +0300

    test

---
+++ ServiceChat/ServiceChat/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceChat
+{
+    [ServiceContract(CallbackContract = typeof(IServiceCallback))]
+    public interface IService
+    {
+        [OperationContract]
+        int Connect(string name);
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract]
+        void SendMessage(string msg);
+    }
+
+    public interface IServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void MessageCallback(string msg);
+    }
+}
---
+++ ServiceChat/ServiceChat/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ServiceModel;
+
+namespace ServiceChat
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public OperationContext operationContext { get; set; }
+    }
+}
